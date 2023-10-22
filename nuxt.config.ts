ba292e52b69daa396399c2665363917410369a07
mattyatea <mattyacocacora0@gmail.com>
2023-10-23 06:15:50 +0900

    ogp

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,4 +8,21 @@ export default defineNuxtConfig({
     fallback: 'dark',
   },
   css: ['/assets/css/tailwind.css'],
+  head: {
+    htmlAttrs: {
+      lang: 'ja',
+      prefix: 'og: http://ogp.me/ns#'
+    },
+    title: 'Misskey デコレーションアイコンプレビュー',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Misskeyのデコレーションアイコンのプレビューが見られるサイトです' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'Misskeyのデコレーションアイコンのプレビューが見られるサイトです' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://icon.nanasi-apps.xyz' },
+      { hid: 'og:title', property: 'og:title', content: 'Misskey decoration icon preview' },
+      { hid: 'og:description', property: 'og:description', content: 'Misskeyのデコレーションアイコンのプレビューが見られるサイトです' },
+    ],
+  },
 })
